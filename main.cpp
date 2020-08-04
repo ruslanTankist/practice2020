56b48d9682842f3b244822df24d22c7bd7f400ea
Юзеев Руслан <yuzeevruslan2000@gmail.com>
2020-08-04 16:23:30 +0300

    first working version

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 #include <vector>
+#include <set>
 
 #include <unistd.h>
 #include <climits>
@@ -14,7 +16,7 @@
 struct tr_str {
     std::string comment;
     std::string declaration;
-    std::string body;
+    std::vector<std::string> body;
 };
 
 struct strings_struct {
@@ -23,7 +25,9 @@ struct strings_struct {
     std::vector<tr_str> classes_list;
 };
 
-void read_as_lines(std::vector<std::string> &in_lines, std::string filename)
+// std::set<std::string> variable_type = {"void", "int", "char", "bool", "float", "double"};
+
+void readAsLines(std::vector<std::string> &in_lines, const std::string filename)
 {
     std::string line;
     int i = 0;
@@ -33,27 +37,110 @@ void read_as_lines(std::vector<std::string> &in_lines, std::string filename)
         while(getline(in, line))
         {
             in_lines.push_back(line);
-            std::cout << i << ' ' << in_lines[i] << std::endl;
             i++;
         }
     }
     in.close();
 }
 
-void Parse(const std::vector<std::string> &lines, strings_struct &res)
+//erasing tabs and spaces
+void eraseSpaces(std::vector<std::string> &lines){
+    for(int i = 0; i < lines.size(); i++){
+        while (lines[i][0] == ' ' || lines[i][0] == '/r')
+            lines[i].erase(0, 1);
+    }
+}
+
+//figuring out the line range of the body
+void parseBrackets(const std::vector<std::string> &lines, int pos, std::vector<std::string> &body)
+{
+    int countOpen = 0, countClose = 0;
+    do{
+        if (pos == lines.size()) {
+            std::cout << "error: more opening brackets than closing\n";
+            break;
+        }
+        body.push_back("");
+        for(int i = 0; i < lines[pos].size(); i++){
+            if(lines[pos][i] == '{')
+                countOpen++;
+            if(lines[pos][i] == '}')
+                countClose++;
+            if(countOpen > 0)
+                body[body.size() - 1] += lines[pos][i];
+        }
+        pos++;
+    } while(countOpen > countClose);
+}
+
+void Parse(std::vector<std::string> &lines, strings_struct &res)
 {
     for(int i = 0; i < lines.size(); i++){
-        while(getchar())
+        //filling the structure
+        if (lines[i].substr(0, 8) == "#include"){
+            res.includes_list.push_back(lines[i]);
+        } else if (lines[i].substr(0, 6) == "struct"){
+            tr_str struct_strings;
+            if (lines[i-1].substr(0, 2) == "//")
+                struct_strings.comment = lines[i-1];
+            struct_strings.declaration = lines[i];
+            parseBrackets(lines, i, struct_strings.body);
+            res.struct_list.push_back(struct_strings);
+        } else if (lines[i].substr(0, 5) == "class"){
+            tr_str struct_strings;
+            if (lines[i-1].substr(0, 2) == "//")
+                struct_strings.comment = lines[i-1];
+            struct_strings.declaration = lines[i];
+            parseBrackets(lines, i, struct_strings.body);
+            res.classes_list.push_back(struct_strings);
+        }
+    }
+}
+
+void printStruct(strings_struct &str){
+    std::cout << "  includes_list:\n";
+    for(int i = 0; i < str.includes_list.size(); i++){
+        std::cout << " include [" << i << "]\n"
+            << str.includes_list[i] << std::endl;
+    }
+
+    std::cout << "\n  struct_list:\n";
+    for(int i = 0; i < str.struct_list.size(); i++){
+        std::cout << " structure [" << i << "]\n"
+            << str.struct_list[i].comment << std::endl
+            << str.struct_list[i].declaration << std::endl;
+        for(int j = 0; j < str.struct_list[i].body.size(); j++) {
+            std::cout << str.struct_list[i].body[j] << std::endl;
+        }
+    }
+
+    std::cout << "\n  classes_list:\n";
+    for(int i = 0; i < str.classes_list.size(); i++){
+        std::cout << " class [" << i << "]\n"
+                  << str.classes_list[i].comment << std::endl
+                  << str.classes_list[i].declaration << std::endl;
+        for(int j = 0; j < str.classes_list[i].body.size(); j++) {
+            std::cout << str.classes_list[i].body[j] << std::endl;
+        }
     }
 }
 
 int main()
 {
     std::vector<std::string> input_lines;
-    read_as_lines(input_lines, "test/sample.h");
+    std::string input_filename;
+    std::cout << "Введите имя .h-файла в директории проекта или введите букву t (test), чтобы использовать test/sample.h" << std::endl;
+    std::cin >> input_filename;
+    if (input_filename == "t")
+        input_filename = "test/sample.h";
+    readAsLines(input_lines, input_filename);
+
+    eraseSpaces(input_lines);
 
     strings_struct res;
     Parse(input_lines, res);
+
+    printStruct(res);
 
     return 0;
 }
---
+++ test/sample.h
@@ -0,0 +1,17 @@
+#include "sample.cpp"
+
+//struct_name structure
+struct struct_name {
+    int variable1;
+};
+
+//Sample class
+class Sample {
+public:
+    int x;
+    char c;
+
+    int foo(int variable2) {
+        return variable2 * 2;
+    }
+};
